01bde981e66c1a007e84081e53098228f0a0ae23
alisarwar253 <alisarwar253@gmail.com>
2026-03-19 14:51:57 +0500

    Scope 2, Scope 3 yearly views added

---
+++ models/gold/api_total_yearly_emissions_scope_3.sql
@@ -0,0 +1,87 @@
+{{ config(
+    materialized='materialized_view'
+) }}
+
+with source as (
+
+    select
+        company_code,
+        site_code,
+        reporting_year,
+        code,
+        code_name,
+        rollup_qty,
+        rollup_emissions,
+        dimensions
+    from {{ ref('cdata_yearly') }}
+    where code like '01-0010-0030-%'
+    and code not in (
+        '01-0010-0030-001',
+        '01-0010-0030-002',
+        '01-0010-0030-018',
+        '01-0010-0030-019',
+        '01-0010-0030-020',
+        '01-0010-0030-021'
+    )
+
+),
+
+aggregated as (
+
+    select
+        company_code,
+        site_code,
+        reporting_year,
+        sum(rollup_qty::numeric) as total_rollup_qty,
+        sum(rollup_emissions::numeric) as total_rollup_emissions,
+
+        -- JSON of contributing codes
+        json_agg(
+            json_build_object(
+                'name', code_name,
+                'value', rollup_qty,
+                'emissions', rollup_emissions,
+                'code', code,
+                'level', 4,
+                'children', coalesce(dimensions, '[]'::jsonb)
+            )
+        ) as level_4_children
+
+    from source
+    group by
+        company_code,
+        site_code,
+        reporting_year
+
+),
+
+final as (
+        select
+        company_code,
+        site_code,
+        reporting_year,
+
+        json_agg(
+            json_build_object(
+                'name', reporting_year,
+                'value', total_rollup_qty,
+                'emission', total_rollup_emissions,
+                'code', '01-0010-0030',
+
+                'children', level_4_children
+            )
+        ) as actual_data    
+    from aggregated
+    group by
+    company_code,
+    site_code,
+    reporting_year
+)
+
+
+select *
+from final
+order by
+    company_code,
+    site_code,
+    reporting_year
---
+++ models/gold/total_yearly_emissions_location_scope_2.sql
@@ -0,0 +1,54 @@
+{{ config(
+    materialized='materialized_view'
+) }}
+
+with source as (
+
+    select
+        company_code,
+        site_code,
+        reporting_year,
+        code,
+        code_name,
+        rollup_qty,
+        rollup_emissions,
+        dimensions
+    from {{ ref('cdata_yearly') }}
+    where code = '01-0010-0020-003'
+
+),
+
+aggregated as (
+
+    select
+        company_code,
+        site_code,
+        reporting_year,
+        sum(rollup_qty::numeric) as total_rollup_qty,
+        sum(rollup_emissions::numeric) as total_rollup_emissions,
+
+        -- JSON of contributing codes
+        json_agg(
+            json_build_object(
+                'code', code,
+                'code_name', code_name,
+                'rollup_qty', rollup_qty,
+                'rollup_emissions', rollup_emissions,
+                'dimensions', dimensions
+            )
+        ) as dimensions_and_codes
+
+    from source
+    group by
+        company_code,
+        site_code,
+        reporting_year
+
+)
+
+select *
+from aggregated
+order by
+    company_code,
+    site_code,
+    reporting_year
